dc73ece719919e8afac205557eb45250e96fd072
Tim Siegler <tim.siegler@fincon.eu>
2022-01-24 20:17:24 +0100

    setup spring backend

---
+++ corona-attendance-report-server/src/main/java/com/mrtimeey/coronaattendancereportserver/CoronaAttendanceReportServerApplication.java
@@ -0,0 +1,13 @@
+package com.mrtimeey.coronaattendancereportserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaAttendanceReportServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronaAttendanceReportServerApplication.class, args);
+	}
+
+}
---
+++ corona-attendance-report-server/src/test/java/com/mrtimeey/coronaattendancereportserver/CoronaAttendanceReportServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrtimeey.coronaattendancereportserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaAttendanceReportServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
