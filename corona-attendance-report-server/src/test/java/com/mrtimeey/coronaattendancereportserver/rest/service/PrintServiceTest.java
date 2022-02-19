e6dd17a872014784d1f67a15bef184cf5e54f50a
Tim Siegler <tim.siegler@fincon.eu>
2022-02-19 23:58:15 +0100

    First simple pdf generator

---
+++ corona-attendance-report-server/src/test/java/com/mrtimeey/coronaattendancereportserver/rest/service/PrintServiceTest.java
@@ -0,0 +1,23 @@
+package com.mrtimeey.coronaattendancereportserver.rest.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PrintServiceTest {
+
+    private PrintService serviceUnderTest;
+
+    @BeforeEach
+    public void setup() {
+        serviceUnderTest = new PrintService();
+    }
+
+    @Test
+    void testPrint() throws Exception {
+        serviceUnderTest.print();
+        assertThat("").isEqualTo("false");
+    }
+
+}
