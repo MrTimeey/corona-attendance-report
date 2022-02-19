1a9a3a132fa3d481824f67fe5461a5a0de63905e
Tim Siegler <tim.siegler@fincon.eu>
2022-02-19 19:37:28 +0100

    Add participants logic

--- corona-attendance-report-server/src/main/java/com/mrtimeey/coronaattendancereportserver/domain/entity/EventParticipant.java
+++ corona-attendance-report-server/src/main/java/com/mrtimeey/coronaattendancereportserver/domain/entity/EventParticipant.java
@@ -1,9 +1,33 @@
 package com.mrtimeey.coronaattendancereportserver.domain.entity;
 
+import com.mrtimeey.coronaattendancereportserver.rest.transfer.PersonTO;
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import javax.validation.constraints.NotBlank;
 
 @Data
 @Builder
 public class EventParticipant {
+
+    @Id
+    private String id;
+
+    @NotBlank
+    private String name;
+
+    @Builder.Default
+    private String address = "";
+
+    @Builder.Default
+    private String phoneNumber = "";
+
+    public static EventParticipant fromTransferObject(PersonTO personTO) {
+        return EventParticipant.builder()
+                .name(personTO.getName())
+                .address(personTO.getAddress())
+                .phoneNumber(personTO.getPhoneNumber())
+                .build();
+    }
 }
--- corona-attendance-report-server/src/main/java/com/mrtimeey/coronaattendancereportserver/rest/service/EventService.java
+++ corona-attendance-report-server/src/main/java/com/mrtimeey/coronaattendancereportserver/rest/service/EventService.java
@@ -1,34 +1,45 @@
 package com.mrtimeey.coronaattendancereportserver.rest.service;
 
 import com.mrtimeey.coronaattendancereportserver.domain.entity.Event;
+import com.mrtimeey.coronaattendancereportserver.domain.entity.EventParticipant;
 import com.mrtimeey.coronaattendancereportserver.domain.repository.EventRepository;
 import com.mrtimeey.coronaattendancereportserver.exception.ResourceNotFoundException;
 import com.mrtimeey.coronaattendancereportserver.rest.request.OnCreate;
 import com.mrtimeey.coronaattendancereportserver.rest.request.OnUpdate;
 import com.mrtimeey.coronaattendancereportserver.rest.transfer.EventTO;
+import com.mrtimeey.coronaattendancereportserver.rest.transfer.TeamTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
 @Validated
 public class EventService {
 
+    private final PersonService personService;
     private final TeamService teamService;
     private final EventRepository eventRepository;
 
 
     @Validated(OnCreate.class)
     public EventTO createEvent(@Valid EventTO eventTO) {
-        if (!teamService.teamExisting(eventTO.getTeamId())) {
-            throw new ResourceNotFoundException(String.format("Event not created! Team with id '%s' not found!", eventTO.getTeamId()));
-        }
+        TeamTO teamTO = teamService.getTeam(eventTO.getTeamId())
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("Event not created! Team with id '%s' not found!", eventTO.getTeamId())));
         Event event = Event.fromTransferObject(eventTO);
+        List<EventParticipant> eventParticipants = teamTO.getMembers().stream()
+                .map(personService::getPerson)
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .map(EventParticipant::fromTransferObject)
+                .collect(Collectors.toList());
+        event.setParticipants(eventParticipants);
         return EventTO.fromBusinessModel(eventRepository.save(event));
     }
 
