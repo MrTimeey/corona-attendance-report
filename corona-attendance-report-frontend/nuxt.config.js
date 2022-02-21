831c68bed15de7534e8ac6b247a1d1f2f0c7b985
tim.siegler.ext <tim.siegler.ext@devk.de>
2022-02-21 15:41:43 +0100

    Nuxt and auth stuff

---
+++ corona-attendance-report-frontend/nuxt.config.js
@@ -0,0 +1,118 @@
+export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'corona-attendance-report-frontend',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    '@nuxtjs/auth-next'
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: '/',
+  },
+  router: {
+    middleware: ['auth']
+  },
+  auth: {
+    strategies: {
+      oidc: {
+        scheme: 'openIDConnect',
+        clientId: 'web',
+        endpoints: {
+          configuration:
+            'http://localhost:8088/auth/realms/corona-attendance-report/.well-known/openid-configuration',
+        },
+        responseType: 'code',
+        grantType: 'authorization_code',
+        scope: ['openid', 'profile', 'offline_access'],
+        codeChallengeMethod: 'S256',
+      },
+      keycloak2: {
+        _scheme: 'oauth2',
+        authorization_endpoint:'https://localhost:8088/user/authorize',
+        userinfo_endpoint: false,
+        access_type: 'offline',
+        access_token_endpoint: 'https://localhost:8088/token',
+        response_type: 'code',
+        token_type: 'Bearer',
+        token_key: 'access_token',
+      },
+      keycloak: {
+        scheme: 'oauth2',
+        endpoints: {
+          authorization: '/auth/realms/corona-attendance-report/protocol/openid-connect/auth',
+          token: '/auth/realms/corona-attendance-report/protocol/openid-connect/token',
+          userInfo: '/auth/realms/corona-attendance-report/protocol/openid-connect/userinfo',
+          logout: '/auth/realms/corona-attendance-report/protocol/openid-connect/logout?redirect_uri=' + encodeURIComponent('http://localhost:3000')
+        },
+        token: {
+          property: 'access_token',
+          type: 'Bearer',
+          name: 'Authorization',
+          maxAge: 300
+        },
+        refreshToken: {
+          property: 'refresh_token',
+          maxAge: 60 * 60 * 24 * 30
+        },
+        responseType: 'code',
+        grantType: 'authorization_code',
+        clientId: 'corona-attendance-report-frontend',
+        scope: ['openid', 'profile', 'email'],
+        codeChallengeMethod: 'S256'
+      }
+    },
+  },
+
+
+  svg: {
+    vueSvgLoader: {
+      // vue-svg-loader options
+    },
+    svgSpriteLoader: {
+      // svg-sprite-loader options
+    },
+    fileLoader: {
+      // file-loader options
+    },
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+}
---
+++ corona-attendance-report-frontend/pages/login.vue
@@ -0,0 +1,39 @@
+<template>
+  <div
+    id="loading-container"
+    class="bg-gray-900 min-h-screen mx-auto my-auto grid grid-cols-1 content-center"
+  >
+    <Spinner
+      id="spinner"
+      fill="#fbbf24"
+      fill-opacity="0.2"
+      class="mx-auto my-auto"
+      width="135px"
+      height="140px"
+    />
+    <p class="text-center text-yellow-400 my-4">Redirect to login...</p>
+  </div>
+</template>
+
+<script>
+import Spinner from '~/assets/bars.svg?inline'
+export default {
+  name: 'IndexPage',
+  components: { Spinner },
+  layout: 'CleanLayout',
+  mounted() {
+    setInterval(this.login, 250)
+  },
+  methods: {
+    async login() {
+      await this.$auth.loginWith('oidc')
+    },
+  },
+}
+</script>
+
+<style scoped>
+#spinner {
+  vertical-align: middle;
+}
+</style>
