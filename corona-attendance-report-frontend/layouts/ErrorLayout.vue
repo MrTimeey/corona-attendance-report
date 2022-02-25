353a525c508eb6e991f150950542a317b161fb97
Tim Siegler <tim.siegler@fincon.eu>
2022-02-25 10:20:59 +0100

    Layouts

---
+++ corona-attendance-report-frontend/layouts/ErrorLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="bg-gray-900 min-h-screen flex flex-col">
+    <div class="max-w-7xl mx-auto block min-h-full py-72">
+      <h1 class="text-yellow-400 text-8xl font-black">404</h1>
+      <p class="text-yellow-400 text-2xl">The page you're looking for can't be found.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ErrorLayout"
+}
+</script>
